3afe61aee9ad2eacd1c9fdf37011ae8e4cbc9175
吴彬 <1368422190@qq.com>
2018-09-21 17:12:22 +0800

    通过BigDecimal实现---->给定四个坐标判断是否构成一个正方形

---
+++ Program_wubin/src/main/java/exercise/JudgeSquare.java
@@ -0,0 +1,12 @@
+package exercise;
+
+/**
+ * @author wubin
+ * @Description
+ * @project Learn-develop
+ * @package exercise
+ * @email wubin326@qq.com
+ * @date 2018/09/21
+ */
+public class JudgeSquare {
+}
